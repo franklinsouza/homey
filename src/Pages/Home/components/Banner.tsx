de9136d3a9d9ca6059f7d20b2136df5edff1f6ed
Franklin Souza <frankrsouza@gmail.com>
2023-03-23 20:35:46 -0300

    Feat: Adiciona formulario na seção de banner

--- src/Pages/Home/components/Banner.tsx
+++ src/Pages/Home/components/Banner.tsx
@@ -1,13 +1,10 @@
 import banner from '../../../assets/39-4.jpg';
-import FormAction from './FormAction';
 import SearchForm from './SearchForm';
 
 const Banner = () => {
   return (
     <div className='flex h-[600px] justify-center items-center bg-center bg-no-repeat bg-cover' style={{backgroundImage: `url(${banner})`}}>
       <div className='container mx-auto px-4'>
-        {/* <img src={banner}/> */}
-        <FormAction />
         <SearchForm />
       </div>
     </div>
--- src/Pages/Home/components/SearchForm.tsx
+++ src/Pages/Home/components/SearchForm.tsx
@@ -1,34 +1,186 @@
-import { X, User } from "lucide-react";
+import { X, User, Minus, Plus, Search } from "lucide-react";
+import { useState } from "react";
 
 const SearchForm = () => {
+  const [ mainForm, setMainForm ] = useState(false);
+  const [ guestsForm, setGuestsForm ] = useState(false);
+  const [ adults, setAdults ] = useState(0);
+  const [ cildren, setChildren ] = useState(0);
+
+  function guestsCounter (counter, operation) {
+    if(counter === 'adults') {
+      setAdults( previousAdults => {
+        const newAdults = previousAdults + operation;
+        return newAdults < 0 ? 0 : newAdults;
+      });
+    }else {
+      setChildren( previousChildren => {
+        const newChildren = previousChildren + operation;
+        return newChildren < 0 ? 0 : newChildren;
+      });
+    }
+  }
+
+  function guestsFormHandle () {
+    setGuestsForm( previousStatus => !previousStatus );
+  }
+
+  function mainFormHandle () {
+    setMainForm( previousMainForm => !previousMainForm );
+  }
+
   return(
-    <div className='bg-white p-5 absolute top-0 right-0 bottom-0 left-0 w-full'>
-      <span className="flex justify-between items-center text-lg font-bold mb-7">
-        Search
-        <X size={22} color={'#3b4249'}/>
-      </span>
-      <form>
-        <select className='w-full mb-3 bg-white border-[1px] border-[#ccc] rounded-md p-4'>
-          <option value='ee'>Athens</option>
-          <option value='ee'>Bramptom</option>
-          <option value='ee'>Bridgend</option>
-        </select>
+    <>
+      <div className='bg-white p-8 rounded-md md:hidden'>
+        <h1 className='text-3xl font-bold mb-3'>
+          Book & Experience Amazing Places
+        </h1>
+        <p className='font-light mb-3'>
+          Wordpress Theme For Booking and Rental
+        </p>
+        <form className='relative mb-1'>
+          <Search color="#4f5962" size={20} strokeWidth={2} className='absolute top-[14px] left-4 pointer-events-none'/>
+          <input 
+            type='text' 
+            className='form-control pl-11' 
+            placeholder='Location'
+            autoComplete="off"
+            onClick={mainFormHandle}
+          />
+        </form>
+      </div>
 
-        <div className='grid grid-cols-2 gap-2 mb-3'>
-          <input type='text' placeholder="Text" className='bg-white border-[1px] border-[#ccc] rounded-md py-3 px-4'/>
-          <input type='text' placeholder="Text" className='bg-white border-[1px] border-[#ccc] rounded-md py-3 px-4'/>
-        </div>
+      <div className={`${mainForm ? 'top-0' : 'top-full'} duration-200 bg-white p-5 rounded-md w-full h-full fixed right-0 bottom-0 left-0 md:p-7 md:relative md:block`}>
 
-        <div className='mb-3'>
-          <User />
-          <input type='text' placeholder="Text" className='bg-white border-[1px] border-[#ccc] rounded-md py-3 px-4 w-full'/>
-        </div>
-
-        <button className='bg-[#f15e75] text-[#FFF] w-full py-3 font-semibold rounded-[5px] text-center'>
+        <span className="flex justify-between items-center text-lg font-bold mb-7 md:hidden">
           Search
-        </button>
-      </form>
-    </div>
+          <X size={22} color={'#3b4249'} onClick={mainFormHandle}/>
+        </span>
+
+        <div className="hidden mb-8 md:block">
+          <span className="font-bold text-3xl mb-4 block">
+            Book & Experience Amazing Places
+          </span>
+          <p className="font-light">
+            WordPress Theme For Booking and Rental
+          </p>
+        </div>
+
+        <form>
+          <div className='mb-3'>
+            <select className='form-control'>
+              <option value='ee'>Athens</option>
+              <option value='ee'>Bramptom</option>
+              <option value='ee'>Bridgend</option>
+            </select>
+          </div>
+
+          <div className='grid grid-cols-2 gap-2 mb-3'>
+            <input type='text' placeholder="Text" className='form-control'/>
+            <input type='text' placeholder="Text" className='form-control'/>
+          </div>
+
+          <div className='mb-3 relative'>
+            <User color="#4f5962" className='absolute top-[12px] left-3'/>
+            <input type='text' placeholder="Guests" className='form-control pl-11' value={adults + cildren} onClick={guestsFormHandle}/>
+
+            <div className={`${guestsForm ? 'block' : 'hidden'} p-5 border-[1px] border-[#ccc] rounded-md absolute w-full bg-white top-[calc(100%_+_12px)]`}>
+              <div className='flex justify-between items-center mb-3'>
+                <div className='flex items-center'>
+                  <span className='font-bold text-lg w-6'>
+                    {adults}
+                  </span> 
+                  <span className="font-light">
+                    Adults
+                  </span>
+                </div>
+                <div className='flex border border-[#54c4d9] w-[88px] h-[44px] rounded-full overflow-hidden'>
+                  <button type="button" className='flex items-center justify-center w-1/2 cursor-pointer group  hover:bg-[#54c4d9] transition-all'>
+                    <Minus 
+                      color='#54c4d9' 
+                      size={20} 
+                      strokeWidth={4} 
+                      className='group-hover:stroke-slate-100 transition-all'
+                      onClick={() => guestsCounter('adults', -1)}
+                    />
+                  </button>
+                  <button type="button" className='flex items-center justify-center w-1/2 cursor-pointer group  hover:bg-[#54c4d9] transition-all'>
+                    <Plus 
+                      color='#54c4d9' 
+                      size={20} 
+                      strokeWidth={4} 
+                      className='group-hover:stroke-slate-100 transition-all'
+                      onClick={() => guestsCounter('adults', 1)}
+                    />
+                  </button>
+                </div>
+              </div>
+
+              <div className='flex justify-between items-center mb-5'>
+                <div className='flex items-center'>
+                  <span className='font-bold text-lg w-6'>
+                    {cildren}
+                  </span>
+                  <span className="font-light">
+                    Children
+                  </span>
+                </div>
+                <div className='flex border border-[#54c4d9] w-[88px] h-[44px] rounded-full overflow-hidden'>
+                  <button type="button" className='flex items-center justify-center w-1/2 cursor-pointer group  hover:bg-[#54c4d9] transition-all'>
+                    <Minus 
+                      color='#54c4d9' 
+                      size={20} 
+                      strokeWidth={4} 
+                      className='group-hover:stroke-slate-100 transition-all'
+                      onClick={() => guestsCounter('children', -1)}
+                    />
+                  </button>
+                  <button type="button" className='flex items-center justify-center w-1/2 cursor-pointer group  hover:bg-[#54c4d9] transition-all'>
+                    <Plus 
+                      color='#54c4d9' 
+                      size={20} 
+                      strokeWidth={4} 
+                      className='group-hover:stroke-slate-100 transition-all'
+                      onClick={() => guestsCounter('children', 1)}
+                    />
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex justify-between items-center mb-5">
+                <span className="font-light">
+                  Pets
+                </span>
+
+                <div className="flex items-center gap-x-3">
+                  <label className="font-light flex items-center gap-x-1">
+                    Yes
+                    <input type="radio" name="pets" value="yes" className="w-5 h-5"/>
+                  </label>
+
+                  <label className="font-light flex items-center gap-x-1">
+                    No
+                    <input type="radio" name="pets" value="no" className="w-5 h-5"/>
+                  </label>
+
+                </div>
+              </div>
+
+              <div className="flex justify-end">
+                <button type='button' className='bg-[#f15e75] text-[#FFF] py-2 px-5 font-semibold rounded-[5px] text-center' onClick={guestsFormHandle}>
+                  Apply
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <button className='bg-[#f15e75] text-[#FFF] w-full py-3 font-semibold rounded-[5px] text-center'>
+            Search
+          </button>
+        </form>
+      </div>
+    </>
+
   )
 }
 
